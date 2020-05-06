a04603cd3ca5d7e875cefbad7ce87862db145516
Jeffry Gonzalez <jeff@hypertheory.com>
2020-05-06 16:09:21 -0400

    Done

---
+++ GrpcClientRoutePlanning/GrpcClientRoutePlanning/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using RoutingGrpcService.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClientRoutePlanning
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new DrivingRouter.DrivingRouterClient(channel);
+
+            var request = new RouteRequest
+            {
+                StreetAddress = "555 Mockingbird Ct",
+                City = "Pasadena",
+                Zip = "44233"
+            };
+
+            var reply = await client.PlanRouteAsync(request);
+
+            Console.WriteLine($"This is {reply.Miles} and should get you there at {reply.DrivingTime.ToDateTime()}");
+            foreach(var step in reply.Steps)
+            {
+                Console.WriteLine($"\t{step}");
+            }
+        }
+    }
+}
---
+++ RoutingGrpcSolution/RoutingGrpcService/Services/RouteService.cs
@@ -0,0 +1,25 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using RoutingGrpcService.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingGrpcService.Services
+{
+    public class RouteService : DrivingRouter.DrivingRouterBase
+    {
+        public override async Task<RouteResponse> PlanRoute(RouteRequest request, ServerCallContext context)
+        {
+
+            var response = new RouteResponse();
+            response.Miles = new Random().Next(15, 200);
+            response.Steps.Add("Go the traffic light");
+            response.Steps.Add("Turn Left");
+            response.Steps.Add("You have arrived");
+            response.DrivingTime = Timestamp.FromDateTime(DateTime.Now.AddMinutes(response.Miles).ToUniversalTime());
+            return response;
+        }
+    }
+}
